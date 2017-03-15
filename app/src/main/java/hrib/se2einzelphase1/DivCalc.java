3f319734d8ba55cad7b4c533dfa5093e84338ffa
hribhrib <chribar@gmx.at>
2017-03-15 11:15:11 +0100

    Getting Started (some Tests to figure out how Android works)

--- app/src/main/java/hrib/se2einzelphase1/DivCalc.java
+++ app/src/main/java/hrib/se2einzelphase1/DivCalc.java
@@ -2,12 +2,29 @@ package hrib.se2einzelphase1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class DivCalc extends AppCompatActivity {
+
+    EditText inputDividend;
+    EditText inputDivosor;
+    TextView output;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_div_calc);
+
+        inputDividend = (EditText) findViewById(R.id.inputDividend);
+        inputDivosor = (EditText) findViewById(R.id.inputDivisor);
+        output = (TextView) findViewById(R.id.output);
+    }
+
+    public void onClickDividieren(View v) {
+    // do something when the button is clicked
+        output.setText(inputDividend.getText());
+
     }
 }
